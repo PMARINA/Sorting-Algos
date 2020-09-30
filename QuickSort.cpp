6e1c599a5bb1ad3c931cff0dbd1af5d3714818a5
PMARINA <bluerat33@gmail.com>
2020-09-29 20:20:15 -0400

    Finished QuickSort
    Signed-off-by: PMARINA <bluerat33@gmail.com>

---
+++ QuickSort.cpp
@@ -0,0 +1,45 @@
+#include <cstdint>
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+int i = 0;
+void print_array(uint64_t* start, uint64_t* end) {
+  // uint64_t* start = s;
+  // uint64_t* end = e;
+  cout << i++ << endl;
+  while (start < end) cout << *(start++) << "\t";
+  cout << endl;
+}
+void run_sort(uint64_t* start, uint64_t* end) {
+  // If recursion done, return
+  if ((end - start) <= 1) return;
+  // print_array(start, end);
+  // Pivot is always the last element (Lomuto)
+  uint64_t pivot_value = *(end - 1);
+  // Do Lomuto Partition-based sorting
+  uint64_t temp;
+  uint64_t* pivot = end;
+  for (uint64_t *read_head = start, *write_head = start; read_head < end;
+       read_head++) {
+    if (*read_head <= pivot_value) {
+      // Swap read and write head values...
+      temp = *read_head;
+      *read_head = *write_head;
+      *write_head = temp;
+      // increment the write head
+      pivot = write_head++;
+    }
+  }
+  // print_array(start, end);
+  // cin >> temp;
+  // Partition and recurse...
+  uint64_t* mid_addr = pivot;
+  run_sort(start, mid_addr);
+  run_sort(mid_addr, end);
+}
+void quicksort(uint64_t* start, uint64_t* end) {
+  print_array(start, end);
+  run_sort(start, end);
+  print_array(start, end);
+}
