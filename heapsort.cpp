5e77139e26e140e156477d28390360f483cb1ba5
PMARINA <bluerat33@gmail.com>
2020-10-05 17:04:31 -0400

    Finished Heapsort!
    Co-authored-by: jrupert731 <joeyrupert731@gmail.com>
    Signed-off-by: PMARINA <bluerat33@gmail.com>

--- heapsort.cpp
+++ heapsort.cpp
@@ -1,98 +1,56 @@
 #pragma once
-//#include "QuickSort.cpp"
+// #include "QuickSort.cpp"
 using namespace std;
-/*
-void makesubheap(uint64_t a[], int i, int n) {
-  uint64_t temp;
 
-  if ((i >= n) || ((2 * i) >= n)) {
-    return;
-  }
-  if (((2 * i) + 1) >= n) {
-    if (a[2 * i] > a[i]) {
-      temp = a[2 * i];
-      a[2 * i] = a[i];
-      a[i] = temp;
-    }
-  } else {
-    if (a[2 * i] > a[(2 * i) + 1]) {
-      if (a[2 * i] > a[2]) {
-        temp = a[2 * i];
-        a[2 * i] = a[i];
-        a[i] = temp;
-      }
-    } else {
-      if (a[(2 * i) + 1] > a[i]) {
-        temp = a[(2 * i) + 1];
-        a[(2 * i) + 1] = a[i];
-        a[i] = temp;
-      }
-    }
-  }
-};
-*/
+void print_array(uint64_t*, uint64_t*);
+void printHeap(uint64_t arr[], int n) {
+  cout << "Array representation of Heap is:\n";
 
-void print_array(uint64_t* , uint64_t* );
+  cout << arr[1] << endl;
+  cout << arr[2] << "\t" << arr[3] << endl;
+  cout << arr[4] << "\t" << arr[5] << "\t" << arr[6] << "\t" << arr[7] << endl;
+  cout << arr[8] << "\t" << arr[9] << "\t" << arr[n] << endl;
+
+  for (int i = 1; i <= n; ++i) cout << arr[i] << " ";
+  cout << "\n";
+}
 
 void heapify(uint64_t a[], int n, int i) {
   uint64_t temp;
-  int largest;
-  int le = (2*i) + 1;
-  int ri = (2*i) + 2;
-  if ((le <= n) && (a[le] > a[i])) {
+  int largest = i;
+  int le = (2 * i);
+  int ri = (2 * i) + 1;
+  if ((le <= n) && (a[le] > a[largest])) {
     largest = le;
-  } else {
-    largest = i;
   }
   if ((ri <= n) && (a[ri] > a[largest])) {
     largest = ri;
   }
   if (largest != i) {
-    temp = a[largest];
-    a[largest] = a[i];
-    a[i] = temp;
+    swap(a[largest], a[i]);
     heapify(a, n, largest);
   }
 }
 
-/*
-void makeheap(uint64_t a[], int n) {
-  for (int i = 1; i <= (n / 2); i++) {
-    makesubheap(a, i, n);
-  }
-};
-*/
-
 void buildheap(uint64_t a[], int n) {
-  for (int i = 1; i <= ((n / 2)); i++) {
+  for (int i = (n / 2) + 1; i >= 1; i--) {
     heapify(a, n, i);
   }
+  // printHeap(a, n);
 }
-/*
-void rebuildheap(uint64_t a[], int n) {
-  uint64_t temp;
-  for (int i = 2; i <= (n - 1); i++) {
-    temp = a[i];
-    a[i] = a[0];
-    a[0] = temp;
-  }
-};
-*/
 
 void heapsort(uint64_t a[], int n) {
   uint64_t temp;
   buildheap(a, n);
-  for (int i = 2; i <= (n); i++) {
-    temp = a[0];
-    a[0] = a[0];
-    a[0] = temp;
-    n = n - 1;
-    heapify(a, n, 1);
+  uint64_t temp_n = n;
+  for (int i = n; i >= 2; i--) {
+    swap(a[1], a[temp_n]);
+    heapify(a, --temp_n, 1);
   }
 }
 
 void heapsort(uint64_t* a, uint64_t* b) {
-  print_array(a, b);
-  heapsort(a, b - a - 1);
-  print_array(a, b);
+  // print_array(a, b);
+  heapsort(a - 1, b - a);
+  // print_array(a, b);
 }
